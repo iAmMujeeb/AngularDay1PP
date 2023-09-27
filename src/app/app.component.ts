755fdc8f623b5fc0a0f100f1da5b51d285e0c80e
Your Name <saifi.mujeeb2@gmail.com>
2023-09-27 17:23:32 +0530

    added: day 1 pp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Helloworld';
+  imgUrl="../assets/logo.jpg";
+  url = "https://www.bridgelabz.com";
+  userName: string = ""; 
+  nameError: string = "";
+
+ngOnInit():void{
+  this.title = "Hello from Bridgelabz";
+}
+
+onClick($event){
+  console.log("Save button is clicked!", $event);
+  window.open(this.url, "_blank");
+}
+
+onInput($event){
+  console.log("Change Event Occured", $event.data);
+  const nameRegex = RegExp('^[A-Z][a-zA-z]{2,}$');
+  if(nameRegex.test(this.userName)){
+    this.nameError = "";
+    return;
+  }
+  this.nameError = "Name is incorrect";
+}
+
 }
